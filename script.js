b3de30d8414a3e617bf0ff423a13289497fda020
Shoxeeeeh <mathiasdavid@live.ca>
2022-11-04 18:44:09 -0300

    api

--- script.js
+++ script.js
@@ -14,3 +14,67 @@ fetch(apiClima)
                 <h1>${data.hourly.temperature_2m[0]}</h1>
                 </div> `;
     })
+
+
+fetch(apiClima)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data.hourly.time[0]);
+
+        let horaActual = new Date;
+
+        let dia = document.getElementById('dia');
+        dia.innerHTML = `
+                    <div>
+                    <h5>${horaActual}</h5>
+                    </div> `;
+    })
+
+fetch(apiClima)
+    .then(Response => Response.json())
+    .then(data => {
+        console.log(data.hourly.temperature_2m[0])
+        let temperatura = document.getElementById('temp');
+        temperatura.innerHTML = `
+                        <div>
+                        <h3>${data.hourly.temperature_2m[19]}</h3>
+                        </div> `;
+    })
+
+fetch(apiClima)
+    .then(Response => Response.json())
+    .then(data => {
+        console.log(data.hourly.apparent_temperature[0])
+        let sens = document.getElementById('sens');
+        sens.innerHTML = `
+                <div>
+                <h3>${data.hourly.apparent_temperature[0]}</h3>
+                </div> `;
+
+    })
+
+
+fetch(apiClima)
+    .then(Response => Response.json())
+    .then(data => {
+        console.log(data.hourly.precipitation[0])
+        let precipitacion = document.getElementById('precip');
+        precipitacion.innerHTML = `
+    <div>
+    <h3>${data.hourly.precipitation[0]}</h3>
+    </div> `;
+
+    })
+
+
+fetch(apiClima)
+    .then(Response => Response.json())
+    .then(data => {
+        console.log(data.hourly.windspeed_10m[0]);
+        let viento = document.getElementById('wind');
+        viento.innerHTML = `
+    <div>
+    <h3>${data.hourly.windspeed_10m[0]}</h3>
+    </div> `;
+
+    })
