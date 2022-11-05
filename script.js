280a788db0fcadbeba017c6954600829f8d73fce
Valeria Sanabria <valerisanabria@tdf.edu.ar>
2022-11-05 10:17:51 -0300

    modif lista

--- script.js
+++ script.js
@@ -3,78 +3,40 @@ const apiClima = "https://api.open-meteo.com/v1/forecast?latitude=-54.82&longitu
 fetch(apiClima)
     .then(res => res.json())
     .then(data => {
-        console.log(data.hourly.time[0]);
-        console.log(data.hourly.temperature_2m[0]);
-        console.log(data.hourly.apparent_temperature[0]);
-
-
-        let mostrar = document.getElementById('test');
-        mostrar.innerHTML = `
-                <div>
-                <h1>${data.hourly.temperature_2m[0]}</h1>
-                </div> `;
-    })
-
-
-fetch(apiClima)
-    .then(res => res.json())
-    .then(data => {
-        console.log(data.hourly.time[0]);
-
+        
+        //Horario
         let horaActual = new Date;
-
         let dia = document.getElementById('dia');
         dia.innerHTML = `
-                    <div>
-                    <h5>${horaActual}</h5>
-                    </div> `;
-    })
+            <div>
+            <h5>${horaActual}</h5>
+            </div> `;
 
-fetch(apiClima)
-    .then(Response => Response.json())
-    .then(data => {
-        console.log(data.hourly.temperature_2m[0])
+            //Temperatura segun horario
         let temperatura = document.getElementById('temp');
         temperatura.innerHTML = `
-                        <div>
-                        <h3>${data.hourly.temperature_2m[19]}</h3>
-                        </div> `;
-    })
+            <div>
+            <h3>${data.hourly.temperature_2m[19]}</h3>
+            </div> `;
 
-fetch(apiClima)
-    .then(Response => Response.json())
-    .then(data => {
-        console.log(data.hourly.apparent_temperature[0])
+            //Sensacion Termica segun horario
         let sens = document.getElementById('sens');
         sens.innerHTML = `
-                <div>
-                <h3>${data.hourly.apparent_temperature[0]}</h3>
-                </div> `;
+            <div>
+            <h3>${data.hourly.apparent_temperature[0]}</h3>
+            </div> `;
 
-    })
-
-
-fetch(apiClima)
-    .then(Response => Response.json())
-    .then(data => {
-        console.log(data.hourly.precipitation[0])
+            //Precipitation segun horario
         let precipitacion = document.getElementById('precip');
         precipitacion.innerHTML = `
-    <div>
-    <h3>${data.hourly.precipitation[0]}</h3>
-    </div> `;
+            <div>
+            <h3>${data.hourly.precipitation[0]}</h3>
+            </div> `;
 
-    })
-
-
-fetch(apiClima)
-    .then(Response => Response.json())
-    .then(data => {
-        console.log(data.hourly.windspeed_10m[0]);
+            //Velocidad del viento segun horario
         let viento = document.getElementById('wind');
         viento.innerHTML = `
-    <div>
-    <h3>${data.hourly.windspeed_10m[0]}</h3>
-    </div> `;
-
+            <div>
+            <h3>${data.hourly.windspeed_10m[0]}</h3>
+            </div> `;
     })
