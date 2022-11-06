92ab9938681bb244755681ac1b6892e62aa2033d
Charro17 <frtus89@hotmail.com>
2022-11-06 16:16:33 -0300

    se agrega funcion para actualizar clima con la hora

--- script.js
+++ script.js
@@ -12,31 +12,47 @@ fetch(apiClima)
             <h5>${horaActual}</h5>
             </div> `;
 
+
+            let ahora = moment().startOf('hour').format('YYYY-MM-DD\THH:mm');
+            console.log(ahora)
+            climaHoy(data,ahora)
+           //funcion para dar ubicacion en el array
+            function climaHoy(data,ahora){
+            let hoy = data.hourly.time.indexOf(ahora);
+            console.log(hoy);
+            return(hoy); //se retorna valor pero se tiene que reasignar fuera de la funcion
+    }
+        
+    let indice = climaHoy(data,ahora);
+
             //Temperatura segun horario
         let temperatura = document.getElementById('temp');
         temperatura.innerHTML = `
             <div>
-            <h3>${data.hourly.temperature_2m[19]}</h3>
+            <h3>${data.hourly.temperature_2m[indice]}°c</h3>
             </div> `;
 
             //Sensacion Termica segun horario
         let sens = document.getElementById('sens');
         sens.innerHTML = `
             <div>
-            <h3>${data.hourly.apparent_temperature[0]}</h3>
+            <h3>${data.hourly.apparent_temperature[indice]}°c</h3>
             </div> `;
 
             //Precipitation segun horario
         let precipitacion = document.getElementById('precip');
         precipitacion.innerHTML = `
             <div>
-            <h3>${data.hourly.precipitation[0]}</h3>
+            <h3>${data.hourly.precipitation[indice]} mm</h3>
             </div> `;
 
             //Velocidad del viento segun horario
         let viento = document.getElementById('wind');
         viento.innerHTML = `
             <div>
-            <h3>${data.hourly.windspeed_10m[0]}</h3>
+            <h3>${data.hourly.windspeed_10m[indice]} Km/h</h3>
             </div> `;
     })
+
+    
+
